448114999b58cc03e52402d9f3188bedc9b51e4d
otaviomart1ns <otavio.martins01@hotmail.com>
2024-03-30 02:18:30 -0300

    feat(config): Adding environment variables

--- config/config.go
+++ config/config.go
@@ -1,5 +1,28 @@
 package config
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DBDriver string
+	DBSource string
+}
+
+func LoadEnv() (*Config, error) {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	dbDriver := os.Getenv("DB_DRIVER")
+	dbSource := os.Getenv("DB_SOURCE")
+
+	return &Config{
+		DBDriver: dbDriver,
+		DBSource: dbSource,
+	}, nil
 }
