d433409772dcad4be7fb43be182a0f4d29bed39e
otaviomart1ns <otavio.martins01@hotmail.com>
2024-03-30 02:19:55 -0300

    feat(utils): Adding function to generate dummy user data

---
+++ utils/random.go
@@ -0,0 +1,53 @@
+package utils
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+var (
+	alphabet     = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	firstNames   = []string{"John", "Michael", "Emily", "Sarah", "David", "Jessica", "Matthew", "Jennifer", "Christopher", "Linda"}
+	lastNames    = []string{"Smith", "Johnson", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Wilson"}
+	emailDomains = []string{"gmail.com", "yahoo.com", "hotmail.com", "outlook.com", "icloud.com"}
+	minDate      = time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC)
+	maxDate      = time.Date(2010, time.December, 31, 23, 59, 59, 0, time.UTC)
+)
+
+func RandomUsernamePassword(length int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < length; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomName() (string, string) {
+	firstName := firstNames[rand.Intn(len(firstNames))]
+	lastName := lastNames[rand.Intn(len(lastNames))]
+	return firstName, lastName
+}
+
+func RandomEmail(firstName, lastName string) string {
+	return fmt.Sprintf("%s.%s@%s",
+		strings.ToLower(firstName),
+		strings.ToLower(lastName),
+		emailDomains[rand.Intn(len(emailDomains))],
+	)
+}
+
+func RandomBirthDate() time.Time {
+	minUnix := minDate.Unix()
+	maxUnix := maxDate.Unix()
+
+	randomUnix := rand.Int63n(maxUnix-minUnix) + minUnix
+	randomBirth := time.Unix(randomUnix, 0)
+
+	return randomBirth
+}
